3e8436d93ea4fba1742d6519e62095e0373e2779
A K Srivastava <cs.abhilive@live.com>
2018-09-13 23:25:30 +0530

    upgraded angular dependencies to newer version

--- app/app.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-/*import { EmployeeListComponent }    from './employee/employee-list.component';*/
-var appRoutes = [
-    { path: '', redirectTo: '/employees', pathMatch: 'full' },
-];
-exports.appRoutingProviders = [];
-exports.routing = router_1.RouterModule.forRoot(appRoutes);
-//# sourceMappingURL=app.routing.js.map
